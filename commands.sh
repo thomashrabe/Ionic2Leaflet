0493720d9c47147268ec8740fac0274b9f385f82
Thomas Hrabe <thrabe@gmail.com>
2017-08-17 22:38:46 -0700

    Ionic 3 update
    Adjust packages for Ionic 3

--- commands.sh
+++ commands.sh
@@ -1,10 +1,10 @@
-ionic start Ionic2Leaflet blank --v2
+ionic start Ionic2Leaflet blank
 
 cd Ionic2Leaflet
 
 npm install leaflet --save
-
 npm install @types/leaflet --save
 
-
+ionic cordova plugin add cordova-plugin-geolocation
+npm install --save @ionic-native/geolocation
 
