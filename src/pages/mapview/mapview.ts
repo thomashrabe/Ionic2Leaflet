2d4b119d36fb145091fd636be0b41681b4567914
Thomas Hrabe <thrabe@gmail.com>
2017-08-17 22:37:56 -0700

    Ionic 3 update
    Adjusting code for Ionic 3

--- src/pages/mapview/mapview.ts
+++ src/pages/mapview/mapview.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import * as Leaflet from "leaflet";
-import { Geolocation } from 'ionic-native';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-mapview',
-  templateUrl: 'mapview.html'
+  templateUrl: 'mapview.html',
+  providers : [Geolocation]
 })
 export class LeafletView {
 
   private _currentLatLng: any;
   private _map: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              private geolocation : Geolocation) {
     
   }
 
@@ -21,7 +23,7 @@ export class LeafletView {
   }
 
   loadMap(){
-    Geolocation.getCurrentPosition({ maximumAge: 60000, timeout: 30000, enableHighAccuracy: true }).then((position) => {
+    this.geolocation.getCurrentPosition({ maximumAge: 60000, timeout: 30000, enableHighAccuracy: true }).then((position) => {
  
         this._currentLatLng = [position.coords.latitude, position.coords.longitude];
        
